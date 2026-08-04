8a4e05c8a56ef9a88f99256c51e4ccc39390c60d
akshyaksh <akshyaaksh118@gmail.com>
2026-08-04 14:48:41 +0530

    traversal.py

---
+++ traversal.py
@@ -0,0 +1,22 @@
+def__init_(self,data):
+   self.data=data
+   self.next=None
+class linked list:
+    def_init_(self):
+    self.head=None
+    def printlist(self):
+      temp=self.head
+      while(temp);
+      print(temp.data)
+      temp=temp.next
+
+
+if__name_=='_main_':
+   llist=linked list()
+   llist.head=Node(1)
+   second=node(2)
+   third=Node(3)
+
+llist.head.next=second:
+second.next=third:
+first.printlist()
