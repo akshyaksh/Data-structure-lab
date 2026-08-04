83cd1158231ea51d3351cb0975f63a2de323837e
akshyaksh <akshyaaksh118@gmail.com>
2026-08-04 14:25:30 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,25 @@
+class node:
+  def__init(self,dataval=None):
+     self.dataval=dataval
+     self.nextval=None
+
+class slinked list:
+def__init_(self):
+   self.headval=None
+    printval is not None:
+      print(printval.dataval)
+      printval+printval.nextval
+
+def atbeginning(self,new data)
+    newnode=node(new data)
+    newnode.nextval=self.headval
+    self.headval=newnode
+
+list=slinkedlist()
+list.headval=node("mon")
+e2=node("tue")
+e3=node("wed")
+list.headval.nextgval=e2
+     e2.nextval=e3
+     list.atbeginning("sun")
+     list.listprint()
